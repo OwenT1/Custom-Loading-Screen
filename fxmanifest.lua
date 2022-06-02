513d631a3f14ca5d3fb5c1c3ef06645043d4b2ff
OwenT1 <106706707+OwenT1@users.noreply.github.com>
2022-06-02 03:12:58 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Judd#7644'
+description 'HorizonRP Loading Screen! By OwenT'
+version '1.0.0'
+
+loadscreen 'html/index.html'
+
+files {
+    'html/index.html',
+	'html/assets/logo.png',
+    'html/css/style.css',
+	'html/js/main.js',
+    'html/assets/bgvideo.mp4',
+	'html/assets/music.mp3'
+}
+
+--loadscreen_manual_shutdown 'yes'
+
+client_script 'client/client.lua'
